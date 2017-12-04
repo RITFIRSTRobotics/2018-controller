ff333620da44e9953eda50bbdb2d1d5ee06d172d
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-04 16:56:11 -0500

    initialize 2018-controller code

---
+++ 2018-controller.ino
@@ -0,0 +1,13 @@
+/**
+ * 2018-controller.ino
+ *
+ * Firmware for the controllers in RIT FIRST's 2018 ImagineRIT project
+ *
+ * @author Connor Henley, @thatging3rkid
+ */
+
+void start() {
+}
+
+void loop() {
+}
