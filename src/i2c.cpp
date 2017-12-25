f1746e44c67e5f2247abb86ab537347309356189
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-25 15:18:10 -0500

    updated transmission

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -16,7 +16,14 @@
 #include <stdio.h>
 #include <stdint.h>
 
-static void __respond(); 
+static void __respond();
+
+// Set the address offset, so that it can be sent to the ASC
+#ifdef ADDRESS
+static uint8_t _addr_offset = ADDRESS;
+#else
+static uint8_t _addr_offset = 0;
+#endif
 
 /**
  * @inherit-doc
@@ -33,10 +40,13 @@ void init_i2c() {
 
   if (digitalRead(ADDR0_PIN) && digitalRead(ADDR1_PIN)) {
     Wire.begin(ADDR2);
+    _addr_offset = 2;
   } else if (digitalRead(ADDR1_PIN)) {
     Wire.begin(ADDR1);
+    _addr_offset = 1;
   } else if (digitalRead(ADDR0_PIN)) {
     Wire.begin(ADDR0);
+    _addr_offset = 0;
   }
 #endif
 
@@ -68,7 +78,7 @@ static void __respond() {
   buttons_rep += ((digitalRead(BUTTON2) != BUTTON2_NEGATED) & 0x01) << 2;
   buttons_rep += ((digitalRead(BUTTON3) != BUTTON3_NEGATED) & 0x01) << 3;
   
-  sprintf(buffer, I2CDATA_FORMAT_STRING, analogRead(JOY0_X) / 4, analogRead(JOY0_Y) / 4, analogRead(JOY1_X) / 4, analogRead(JOY1_Y) / 4, buttons_rep);
+  sprintf(buffer, I2CDATA_FORMAT_STRING, _addr_offset, analogRead(JOY0_X) / 4, analogRead(JOY0_Y) / 4, analogRead(JOY1_X) / 4, analogRead(JOY1_Y) / 4, buttons_rep);
   Wire.write(buffer);
 }
 
